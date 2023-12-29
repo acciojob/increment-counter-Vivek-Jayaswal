8834a73c41221a64b498b8f566a316f9da921d1a
Vivek-Jayaswal <141914455+Vivek-Jayaswal@users.noreply.github.com>
2023-12-29 15:33:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const button = document.getElementById("btn");
+const para =  document.querySelector("p");
+
+let count = 0;
+button.addEventListener("click", ()=>{
+	count++;
+	para.innerText = count;
+});
